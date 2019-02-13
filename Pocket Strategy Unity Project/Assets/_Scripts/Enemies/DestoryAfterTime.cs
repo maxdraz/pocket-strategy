bd64c855a1590cf8f0c839b3d6b6fa24a7edd8f0
Max <38158634+maxdraz@users.noreply.github.com>
2019-02-13 13:21:39 +0000

    Changes in class

--- Pocket Strategy Unity Project/Assets/_Scripts/Enemies/DestoryAfterTime.cs
+++ Pocket Strategy Unity Project/Assets/_Scripts/Enemies/DestoryAfterTime.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class DestoryAfterTime : MonoBehaviour
 {
+
+    public float time;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(DestroyAfterT(time));
     }
 
     // Update is called once per frame
@@ -15,6 +18,8 @@ public class DestoryAfterTime : MonoBehaviour
     {
         
     }
+
+    
 
     IEnumerator DestroyAfterT(float t)
     {
