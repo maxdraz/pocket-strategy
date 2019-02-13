981da6403949938373dfdca2b54b80648b69e4d9
PKdude96 <C16452312@mydit.ie>
2019-02-13 02:51:13 +0000

    Fix Main Menu nav

---
+++ Pocket Strategy Unity Project/Assets/SceneNav.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneNav : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LoadHowToPlayScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainLevelScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
