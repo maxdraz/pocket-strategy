7743320288bb2ceb9f0f565d8b37dff473bc6bfb
Max <38158634+maxdraz@users.noreply.github.com>
2019-02-14 14:37:10 +0000

    energy orb travelling

--- Pocket Strategy Unity Project/Assets/_Scripts/Energy/EnergyOrb.cs
+++ Pocket Strategy Unity Project/Assets/_Scripts/Energy/EnergyOrb.cs
@@ -20,10 +20,7 @@ public class EnergyOrb : MonoBehaviour
             Debug.Log("moving to target");
             agent.SetDestination(target.position);
 
-            if (agent.remainingDistance <= 0.1f)
-            {
-                Destroy(gameObject);
-            }
+            
         }
 
         
